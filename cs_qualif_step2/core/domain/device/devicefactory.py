527becdb7c1d876203ca9b67296940df769b83b2
Félix-Olivier Noël <felix.olivier06@gmail.com>
2025-10-14 20:44:23 -0400

    added regex de malade en fait

--- cs_qualif_step2/core/domain/device/devicefactory.py
+++ cs_qualif_step2/core/domain/device/devicefactory.py
@@ -4,6 +4,8 @@ import re
 from cs_qualif_step2.core.domain.device.device import Device
 from cs_qualif_step2.core.domain.device.device_id import DeviceId
 from cs_qualif_step2.core.domain.device.exception.invalid_mac_adress import InvalidMacAddress
+from cs_qualif_step2.core.domain.device.exception.invalid_firmware import InvalidFirmware
+
 from cs_qualif_step2.core.application.dto.device_config import DeviceConfig
 
 
@@ -11,11 +13,12 @@ class DeviceFactory:
     def create_device(self, device_config: DeviceConfig) -> Device:
         if not re.match(r'^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$', device_config.macAddress):
             raise InvalidMacAddress("Invalid MAC address format")
-
-        device_id = DeviceId.generate()
+        # Regex de fou (oui il est valide)
+        if not re.match(r'^[0-9].[0-9].[0-9]$', device_config.firmwareVersion):
+            raise InvalidFirmware("Invalid Firmware Version")
 
         return Device(
-            device_id=device_id,
+            device_id=device_config.device_id,
             macAddress=device_config.macAddress,
             model=device_config.model,
             firmwareVersion=device_config.firmwareVersion,
