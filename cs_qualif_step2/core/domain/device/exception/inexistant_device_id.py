2f42f3f9f467b56692cec4e3a657d9a19541d260
Félix-Olivier Noël <felix.olivier06@gmail.com>
2025-10-14 20:50:34 -0400

    Antony martel

--- cs_qualif_step2/core/domain/device/exception/inexistant_device_id.py
+++ cs_qualif_step2/core/domain/device/exception/inexistant_device_id.py
@@ -1,7 +1,7 @@
-from cs_qualif_step2.core.domain.exception.ConflictException import ConflictException
+from cs_qualif_step2.core.domain.exception.ConflictException import RequestValidationError
 
 
-class InexistantDeviceId(ConflictException):
+class InexistantDeviceId(RequestValidationError):
     def __init__(self, device_id: str):
         self.device_id = device_id
         super().__init__(f"Device id {device_id} not found.")
